0fe0f674910d4e3e657b09bb03bcaf06f06a6b3b
RA21708452 <RA21708452@AN0700102077506.dinfor.uniceub>
2017-11-24 19:47:10 -0200

    Commit numero 2

--- src/control/ControlExibirMusica.java
+++ src/control/ControlExibirMusica.java
@@ -7,5 +7,13 @@ public class ControlExibirMusica {
 	//Atributo
 	private BD bd;
 	private ViewExibirMusica vem;
+	
+	//Metodo construtor
+	public ControlExibirMusica(BD bd){
+		this.bd = bd;
+		//Instanciar a view
+		this.vem = new ViewExibirMusica();
+	}
+	//Metodo que recupera os Cliente e exibe
 
 }
